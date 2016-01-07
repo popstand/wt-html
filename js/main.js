d3f97c037f2cd46287ab89003dbe311452434b02
Chris Whitman <chris@whitmanconsulting.com>
2016-01-07 10:22:42 -0800

    Social media sharing icons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function(){
+    // Listen for click to toggle visibility of additional sharing networks
+    $('.show-more, .show-less').on('click', function(){
+        $(this).hide()
+            .siblings('.show-more, .show-less')
+            .css('display', 'inline-block');
+        $('.secondary-networks').toggleClass('active');
+    })
+})
