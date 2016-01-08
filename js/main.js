d523f1b5cc57bf7500d069467e3d279d9b175b0e
Chris Whitman <chris@whitmanconsulting.com>
2016-01-07 19:45:14 -0800

    Show social sharing buttons when scrolling beyond the static-placed sharing buttons on article page

--- js/main.js
+++ js/main.js
@@ -1,9 +1,40 @@
+//
+// For article_page.html
+//
+// Listen for click to toggle visibility of additional sharing networks
 $(document).ready(function(){
-    // Listen for click to toggle visibility of additional sharing networks
     $('.show-more, .show-less').on('click', function(){
         $(this).hide()
             .siblings('.show-more, .show-less')
             .css('display', 'inline-block');
         $('.secondary-networks').toggleClass('active');
     })
+
+    // Show sharing menu on header
+
 })
+
+//
+// For article_page.html
+//
+// Place social sharing button in header at certain point
+// when srolling by showing/hiding share buttons in the
+// main (header) navbar when scrolling beyond the share
+// buttons on the article page itself.
+$(window).on('scroll', function() {
+    var yScrollPos = window.pageYOffset;
+    var scrollPosToShowNavbarSharing = $sharingEl.position().top + $sharingEl.height();
+
+    var $sharingEl = $('.post-container .share-btn-group');
+    var $categoryNavEl = $('.navbar-article-categories');
+    var $socialNavEl = $('.navbar-social');
+
+    if(yScrollPos > scrollPosToShowNavbarSharing) {
+        $categoryNavEl.hide();
+        $socialNavEl.show();
+    }
+    if(yScrollPos < scrollPosToRelocateAt) {
+        $categoryNavEl.show();
+        $socialNavEl.hide();
+    }
+});
